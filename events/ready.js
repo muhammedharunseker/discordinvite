01d1214b1f486425570ac8e7b8502be12faaf0a3
muhammedharunseker <70948113+muhammedharunseker@users.noreply.github.com>
2021-01-12 21:59:06 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+var prefix = ayarlar.prefix;
+
+module.exports = async client => {
+  var oyun = ["Hayunn ♥ Ponçik"];
+
+  setInterval(async () => {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], { type: "WATCHING" });
+  }, 12000);
+  client.user.setStatus("online");
+};
